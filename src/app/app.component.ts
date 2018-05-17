0644dd8cbc6b82c61870eeb91a4cffedda89f7ef
rovsheneko <rovshennazarov@gmail.com>
2018-05-16 22:45:41 -0500

    test
    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface Point {
+  x: number;
+  y: number;
+}
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
-  name = 'Angular 6';
+
+  name = 1;
+
+  drawPoint (point: Point) {
+    this.name =  point.x + point.y + this.name;
+  }
 }
